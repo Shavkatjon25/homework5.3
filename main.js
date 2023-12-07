34bedb44654695354e4e8652a57cd31e47c00496
Shavkatjon25 <shavkatjona75@gmail.com>
2023-12-07 09:53:04 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="q"){let x =new Audio();
+        x.src="./808_6.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="a"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_S.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="z"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_O.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="p"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_C.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="l"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_7.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="m"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_5.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="t"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_4.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="g"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_3.wav"
+        x.play();}
+})
+document.addEventListener("keypress", Event=>{
+    if(Event.key=="v"){let x =new Audio();
+        x.src="./808-Drum-Kit/808_2.wav"
+        x.play();}
+})
